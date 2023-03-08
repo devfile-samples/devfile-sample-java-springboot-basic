017ded3d61bbc98206a8733b50ce5bca6d6415d0
Cuiping Huo <46670803+cuipinghuo@users.noreply.github.com>
2023-03-08 11:56:47 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello from chuo!";
     }
 
     public static void main(String[] args) {
