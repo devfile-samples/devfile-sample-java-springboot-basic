10e60c7570af0b9a65c6502bdb2f1eb39d886858
SimoChami <simo.chami@cgi.com>
2023-04-13 16:05:24 -0400

    added missing provider for encryption

--- src/main/java/ca/bc/gov/chefs/etl/ChefsETLMainMethod.java
+++ src/main/java/ca/bc/gov/chefs/etl/ChefsETLMainMethod.java
@@ -3,8 +3,10 @@ package ca.bc.gov.chefs.etl;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.Security;
 
 import org.apache.camel.main.Main;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import ca.bc.gov.chefs.etl.constant.Constants;
 import ca.bc.gov.chefs.etl.core.routes.HelloRoute;
@@ -31,6 +33,7 @@ public class ChefsETLMainMethod {
 
 	public static void main(String... args) throws Exception {
 		Main main = new Main();
+		Security.addProvider(new BouncyCastleProvider());
 		main.configure().addRoutesBuilder(HelloRoute.class);
 		main.configure().addRoutesBuilder(AIMSFormRoute.class);
 		main.configure().addRoutesBuilder(FacilityFormRoute.class);
