718bae08abe114c7fe4bfca77912794f6fc0e2b9
Evgeny Pluzhnikov <evgeny.pluzhnikov@cgi.com>
2023-04-17 12:47:54 -0700

    Removing calculation for directory that contains pgp public key. Instead the app will rely on the value of PUBLIC_KEY_NAME_PROPERTY property.

--- src/main/java/ca/bc/gov/chefs/etl/util/FileUtil.java
+++ src/main/java/ca/bc/gov/chefs/etl/util/FileUtil.java
@@ -249,10 +249,6 @@ public class FileUtil {
 		return PropertiesUtil.getValue(propertyName);
 	}
 
-	public static String buildPublicKeyPath(String propertyName){
-		return "src/main/resources/"+PropertiesUtil.getValue(propertyName);
-	}
-
 	/** ----------------decryption starts here, testing encryption -------------------
 	* TODO This part is for testing purposes only, remove when done */
 
