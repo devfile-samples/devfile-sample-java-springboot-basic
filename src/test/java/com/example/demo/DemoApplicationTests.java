881276ece04a6fd3ce7c24d94c60a2ceb18a330a
Mousaei <vahidmfr@yahoo.fr>
2023-06-17 21:35:18 +0200

    Mise a jour pom.xml Commentaire test class

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DemoApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
