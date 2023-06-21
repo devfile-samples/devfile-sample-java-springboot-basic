2f3540b8a68b931a5a0f5b2ca0c22f41fdd4c959
Mousaei <vahidmfr@yahoo.fr>
2023-06-21 21:14:16 +0200

    Update ServletInitializer.java

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.surface.demo;
+package com.example.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
